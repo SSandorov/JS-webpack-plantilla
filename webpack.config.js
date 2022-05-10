36bf82fe9d90da6bb6bb140ba66a580fe9894c35
ssandorov <stefansandorovssb@gmail.com>
2022-05-10 16:35:26 +0200

    configuración webpack en modo de producción

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
 // Archivo de configuración para el webpack
+
+// Para poder mandar a producción los archivos HTML empleamos el siguiente plugin
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
 
+    output: {
+        clean: true
+    },
+
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            }
+        ]
     },
 
     optimization: {},
 
-    plugins: []
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Plantilla Webpack',
+            template: './src/index.html'
+        })
+    ]
 }
